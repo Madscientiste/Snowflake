ed6037cabd9dac834e20a6c63ca5fe7f53c7c796
Madscentist <s.shaade77@gmail.com>
2021-08-17 17:32:20 +0200

    refactoring

--- snowflake.py
+++ snowflake.py
@@ -1,94 +1,112 @@
 from datetime import datetime
-from itertools import count
 import time
 
+from timeit import default_timer as timer
+from datetime import timedelta
 
-class snowflake:
-    increment_bits = 10
-    increment_mask = -1 ^ (-1 << increment_bits)
 
-    instance_id_bits = 5
-    instance_id_mask = -1 ^ (-1 << instance_id_bits)
+class Snowflake:
+
+    # Saturday, 1 January 2011 12:00:00 GMT+01:00
+    # Creation of actimeo
+    initial_epoch = 1293879600000
+    mask = lambda x: -1 ^ (-1 << x)
+    sleep = lambda x: time.sleep(x / 1000)
+    get_timestamp = lambda x: int(time.time() * 1000)
+
+    sequence_bits = 10
+    sequence_mask = mask(sequence_bits)
+
+    instance_bits = 6
+    instance_mask = mask(instance_bits)
+    instance_shift = sequence_bits
     instance_id = 0
-    instance_id_shift = increment_bits
 
-    process_id_bits = 10
-    process_id_mask = -1 ^ (-1 << process_id_bits)
-    process_id_shift = instance_id_bits
+    process_bits = 6
+    process_mask = mask(instance_bits)
+    process_shift = sequence_bits + instance_bits
+    process_id = 0
 
     timestamp_bits = 42
-    timestamp_shift = instance_id_bits + process_id_bits
-    timestamp_mask = -1 ^ (-1 << timestamp_bits)
+    timestamp_mask = mask(timestamp_bits)
+    timestamp_shift = process_bits + sequence_bits + instance_bits
+    timestamp = -1
 
-    sleep = lambda x: time.sleep(x / 1000.0)
+    def __new__(cls, *args, **kwds):
+        cls.instance_id = (cls.instance_id + 1) & cls.instance_mask
+        return super(Snowflake, cls).__new__(cls, *args, **kwds)
 
-    def __init__(self, value=None, process_id=0) -> None:
-        super().__init__()
-        snowflake.instance_id = (snowflake.instance_id + 1) & self.instance_id_mask
-        self.process_id = process_id & self.process_id_mask
-        self.increment = 0
+    def __init__(self, snowflake=0, process_id=0):
+        self.snowflake = snowflake
+        self.sequence = 0
 
-        self.__value = next(self) if not value else int(value)
+    def __int__(self):
+        return self.snowflake
+
+    def __str__(self):
+        return f"{self.snowflake}"
 
     def __repr__(self):
-        return f"snowflake('{self.__value}')"
-
-    def __iter__(self):
-        return self.__next__()
+        return f"{self.__class__.__name__}({str(self)})"
 
     def __next__(self):
-        self.increment = (self.increment + 1) & self.increment_mask
-        return self.__generate__()
+        self.sequence = (self.sequence + 1) & self.sequence_mask
+        return self
 
-    def __generate__(self):
+    def __iter__(self):
         last_timestamp = -1
 
         while True:
-            self.increment = (self.increment + 1) & self.increment_mask
-            timestamp = int(time.time() * 1000)
+            timestamp = self.get_timestamp()
 
             if last_timestamp > timestamp:
                 print("clock is moving backwards. waiting until {last_timestamp}")
                 self.sleep(last_timestamp - timestamp)
                 continue
 
+            if last_timestamp == timestamp:
+                next(self)
+            else:
+                self.sequence = 0
+
+            print(self.sequence)
+
             last_timestamp = timestamp
 
             b_timestamp = (timestamp - self.initial_epoch) << self.timestamp_shift
-            b_process_id = (self.process_id & self.process_id_mask) << self.process_id_shift
-            b_increment = self.increment & self.instance_id_bits
-            b_instance_id = self.instance_id
+            b_process = (self.process_id & self.process_mask) << self.process_shift
+            b_instance = (self.instance_id & self.instance_mask) << self.instance_shift
+            b_sequence = self.sequence & self.sequence_mask
 
-            yield b_timestamp | b_process_id | b_instance_id | b_increment
+            yield b_timestamp | b_process | b_instance | b_sequence
 
     @property
     def timestamp(self):
-        return float((self.__value >> 22) + self.initial_epoch) / 1000
+        return float((self.snowflake >> 22) + self.initial_epoch) / 1000
 
     @property
-    def date(self):
-        return datetime.fromtimestamp(self.timestamp).strftime("%d-%m-%Y | %H:%M:%S")
+    def to_date(self, format="%d-%m-%Y | %H:%M:%S"):
+        return datetime.fromtimestamp(self.timestamp).strftime(format)
 
     @property
     def to_binary(self):
-        return format(self.__value, "08b")
+        return format(self.snowflake, "08b")
 
 
-tobin = lambda x: format(x, "08b")
-# print(id.to_binary, len(id.to_binary))
+# tobin = lambda x: format(x, "08b")
 
-for snow in snowflake():
-    print(snow)
+timeout = 1
+timeout_start = time.time()
+ids = []
 
+start = timer()
+for snowflake in Snowflake():
+    ids.append(snowflake)
+    # print(snowflake)
+    if time.time() > timeout_start + timeout:
+        break
 
-for x in range(50):
-    id = snowflake()
-    print("x", id, id.process_id)
+end = timer()
 
-    for y in range(10):
-        id = snowflake(process_id=1)
-        print("y", id, id.process_id)
-
-        for z in range(10):
-            id = snowflake(process_id=2)
-            print("z", id, id.process_id)
+print(len(ids))
+print(timedelta(seconds=end - start))
